027b64be8cb36e693b37e646731a5a8c136e8956
Roman Cherkasov <chero858@gmail.com>
2020-08-12 17:44:11 +0300

    edit format of saving jsons

--- foo.py
+++ foo.py
@@ -5,15 +5,10 @@ import json
 import os
 
 
-# response = requests.get('http://egr.gov.by/api/v2/egr/getAddressByRegNum/100059271')
-# tp1 = requests.get("http://egr.gov.by/egrn/API.jsp?TP=1&MASK=01000000000000000")
-# data = json.loads(response.text)
-#
-# print(tp1.text)
-
-
 class EgrParser:
     def __init__(self):
+        # self.ip_regnums = regnums2
+        # self.jur_regnums = regnums1
         with open(f'{os.path.dirname(os.path.abspath(__file__))}\\ip.json') as f:
             self.ip_regnums = [param['NM'] for param in json.load(f)]
         with open(f'{os.path.dirname(os.path.abspath(__file__))}\\jur.json') as f:
@@ -35,28 +30,18 @@ class EgrParser:
             json.dump(urls, f, ensure_ascii=False)
 
     def get_ip_jsons_by_regnum(self, regnum):
-        jsons = {}
+        jsons = []
         for method in self.common_methods + self.ip_methods:
             method_json = requests.get(f'http://egr.gov.by/api/v2/egr/{method}/{regnum}').json()
-            # with open(f'{os.path.dirname(os.path.abspath(__file__))}\\{method}.json', 'w', encoding="utf-8") as f:
-            #     json.dump(method_json, f, indent=2, ensure_ascii=False)
-            jsons += method_json if isinstance(method, list) else [method_json, ]
+            jsons += method_json if isinstance(method_json, list) else [method_json, ]
         return jsons
 
     def get_jur_jsons_by_regnum(self, regnum):
         jsons = []
         for method in self.common_methods + self.jur_methods:
-            print(f'http://egr.gov.by/api/v2/egr/{method}/{regnum}')
             method_json = requests.get(f'http://egr.gov.by/api/v2/egr/{method}/{regnum}').json()
-            # with open(f'{os.path.dirname(os.path.abspath(__file__))}\\{method}.json', 'w', encoding="utf-8") as f:
-            #     json.dump(method_json, f, indent=2, ensure_ascii=False)
-            jsons += method_json if isinstance(method, list) else [method_json, ]
+            jsons += method_json if isinstance(method_json, list) else [method_json, ]
         return jsons
-        # final_list = []
-        # for elem in jsons:
-        #     if elem not in final_list:
-        #         final_list.append(elem)
-        # return final_list
 
     def get_jsons(self):
         jsons = []
@@ -67,6 +52,7 @@ class EgrParser:
             json_by_regnum = self.get_jur_jsons_by_regnum(regnum)
             jsons.append(json_by_regnum)
         return jsons
+
 
 def main():
     # egr = EgrParser([700008856, 100059271, 100098469, 190835473], [192201275])
--- test.py
+++ test.py
@@ -7,21 +7,22 @@ import codecs
 with open('urls.json', 'r', encoding="utf-8") as f:
     urls = json.load(f)
 
-main_list = []
+
+main_list = {}
 
 
 async def get(url):
-    responses_list = {url: None}
+    regnum = int(url.split('/')[-1])
+    if not main_list.get(regnum):
+        main_list[regnum] = []
     try:
         connector = aiohttp.TCPConnector(limit=50)
         async with aiohttp.ClientSession(connector=connector) as session:
             async with session.get(url=url, headers={'accept': '*/*'}) as response:
                 resp = await response.json()
-                responses_list[url] = resp
-                main_list.append(responses_list)
+                main_list[regnum] += resp if isinstance(resp, list) else [resp, ]
     except Exception as e:
         print("Unable to get url {} due to {}.".format(url, e.__class__))
-        main_list.append(responses_list)
 
 
 async def main(urls):
@@ -38,6 +39,11 @@ if __name__ == '__main__':
         end = time.time()
         prev = i
         print("Took {} seconds to pull {} websites.".format(end - start, len(main_list)))
+    asyncio.run(main(urls[prev:]))
+    # start = time.time()
+    # asyncio.run(main(urls))
+    # end = time.time()
+    # print("Took {} seconds to pull {} websites.".format(end - start, len(main_list)))
 
     with codecs.open('data1.json', 'w', 'utf-8') as f:
         json.dump(main_list, f, ensure_ascii=False)
